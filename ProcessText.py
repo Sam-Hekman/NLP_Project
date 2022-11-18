db6b2a3b864d100adebf826f42f2933a1c9fbbf9
emergencybutler <49498155+emergencybutler@users.noreply.github.com>
2022-11-17 22:05:19 -0500

    Changing review count, adding scores
    - Changed from a max of 5000 reviews to a max of 5100 (1700 in each class)
    - Added method for getting precision, recall, and F1 given the true and predicted values
    - Now displays number of tokens, positive, negative, and neutral classes after train-test split

--- ProcessText.py
+++ ProcessText.py
@@ -9,6 +9,7 @@ from nltk.stem import WordNetLemmatizer
 from nltk import word_tokenize, pos_tag
 from nltk.corpus import wordnet
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, precision_score, recall_score
 
 # region variables
 categories = ["AMAZON_FASHION", "All_Beauty", "Appliances", "Arts_Crafts_and_Sewing", "Automotive", "Books",
@@ -22,15 +23,19 @@ categories = ["AMAZON_FASHION", "All_Beauty", "Appliances", "Arts_Crafts_and_Sew
               "Toys_and_Games",
               "Video_Games"]  # the categories in the Amazon review dataset
 trainPercent = .7  # percentage of data to use for training
-maxReviews = 5000  # maximum number of reviews to get from a category
+maxReviews = 1700  # maximum number of reviews to get from a category
 # Leave at none to use the global random state instance from numpy.random, use an integer for a reproducible output
-splitRandomSeed = 2
+splitRandomSeed = 1
+negativeCutoff = 2.0
+neutralCutoff = 4.0
+positiveCutoff = 5.0
 
 # file locations
 reviewsFile = "datasetProcessing\\reviews.json.gz"
 tokenizedCleanedFile = "datasetProcessing\\tokenizedCleaned.json.gz"
 stemmedFile = "datasetProcessing\\stemmed.json.gz"
 lemmatizedFile = "datasetProcessing\\lemmatized.json.gz"
+labels = ["positive", "neutral", "negative"]
 
 
 # endregion
@@ -47,7 +52,9 @@ def ImportDataset():
         for category in categories:  # go through all of the categories
             fileName = category + "_5.json.gz"
             if not os.path.exists('dataset\\' + fileName):  # verify we don't already have the file
-                fileDownload = "http://deepyeti.ucsd.edu/jianmo/amazon/categoryFilesSmall/" + fileName
+                print(fileName + " downloading")
+                # fileDownload = "http://deepyeti.ucsd.edu/jianmo/amazon/categoryFilesSmall/" + fileName
+                fileDownload = "http://jmcauley.ucsd.edu/data/amazon_v2/categoryFilesSmall/" + fileName
                 urlretrieve(fileDownload, 'dataset\\' + fileName)  # download the file
 
 
@@ -56,7 +63,10 @@ def GetDataset():
     dataset = {}
     for category in categories:
         reviews = []  # list of [reviewText, rating] lists for this category
-        curReviews = 0  # counter to check we don't pass maxReviews
+        curNegativeReviews = 0  # counter to check we don't pass maxReviews
+        curNeutralReviews = 0
+        curPositiveReviews = 0
+        curReviews = 0
         file = category + "_5.json.gz"  # create file name
         content = gzip.open(os.path.join('dataset', file), 'r')  # get the content from the .json.gz file
 
@@ -65,10 +75,22 @@ def GetDataset():
             js = json.loads(line)  # convert line to json
             if "reviewText" in js:  # only keep reviews in dataset that have review text - excludes ones that are only ratings
                 reviewInfo = [js["reviewText"].lower(), js["overall"]]  # get tuple (reviewText, rating)
+                score = reviewInfo[1]
                 if reviewInfo not in reviews:  # only keep this review if it isn't a duplicate
-                    reviews.append(reviewInfo)
-                    curReviews += 1
-            if curReviews == maxReviews:  # stop going through the file if we reach max reviews
+                    # check which category the review falls in and only keep it if that class hasn't exceeded its limit
+                    if score <= negativeCutoff and curNegativeReviews < maxReviews:
+                        curNegativeReviews += 1
+                        reviews.append(reviewInfo)
+                        curReviews += 1
+                    elif score <= neutralCutoff and curNeutralReviews < maxReviews:
+                        curNeutralReviews += 1
+                        reviews.append(reviewInfo)
+                        curReviews += 1
+                    elif score <= positiveCutoff and curPositiveReviews < maxReviews:
+                        curPositiveReviews += 1
+                        reviews.append(reviewInfo)
+                        curReviews += 1
+            if curReviews == maxReviews * 3:  # stop going through the file if we reach max reviews
                 break
 
         dataset[category] = reviews
@@ -167,6 +189,46 @@ def CreateJsonFile(filePath: str, data):
         fout.write(jsonBytes)
 
 
+def GetDatasetStats(training, testing):
+    print("-------- Stats --------")
+    tokenCountTrain = 0
+    posCountTrain = 0
+    neutralCountTrain = 0
+    negCountTrain = 0
+    tokenCountTest = 0
+    posCountTest = 0
+    neutralCountTest = 0
+    negCountTest = 0
+    for category in training:
+        for reviews in training[category]:
+            tokenCountTrain += len(reviews[0])
+            if reviews[1] < negativeCutoff:
+                negCountTrain += 1
+            elif reviews[1] < neutralCutoff:
+                neutralCountTrain += 1
+            elif reviews[1] < positiveCutoff:
+                posCountTrain += 1
+        for reviews in testing[category]:
+            tokenCountTest += len(reviews[0])
+            if reviews[1] < negativeCutoff:
+                negCountTest += 1
+            elif reviews[1] < neutralCutoff:
+                neutralCountTest += 1
+            elif reviews[1] < positiveCutoff:
+                posCountTest += 1
+
+    print("Training data: ")
+    print("\tTokens: {0}".format(tokenCountTrain))
+    print("\tNegative Reviews: {0}".format(negCountTrain))
+    print("\tNeutral Reviews: {0}".format(neutralCountTrain))
+    print("\tPositive Reviews: {0}".format(posCountTrain))
+    print("\nTest data: ")
+    print("\tTokens: {0}".format(tokenCountTest))
+    print("\tNegative Reviews: {0}".format(negCountTest))
+    print("\tNeutral Reviews: {0}".format(neutralCountTest))
+    print("\tPositive Reviews: {0}".format(posCountTest))
+
+
 # combine all data, then split into training and testing
 # if stem is True, returns stemmed data, otherwise returns lemmatized
 # returns train data, test data
@@ -181,6 +243,7 @@ def SplitData(stem: bool):
     # split the data, giving the trainPercent and random seed
     split = train_test_split(allData, train_size=trainPercent, random_state=splitRandomSeed)
 
+    GetDatasetStats(split[0], split[1])
     return split[0], split[1]  # return train data, test data
 
 
@@ -203,4 +266,30 @@ def SplitDataKeepCategories(stem: bool):
         train[category] = split[0]  # update training and testing dictionary for this category
         test[category] = split[1]
 
+    GetDatasetStats(train, test)
     return train, test
+
+
+# returns precision, recall, and F1 score (in order) for each class in the matrix as a dictionary
+def GetScores(y_true, y_pred):
+    stats = {}  # dictionary to hold the calculations for each class
+
+    # getting precision, recall, and f1 for all classes
+    precision = precision_score(y_true, y_pred, average=None, labels=labels)
+    recall = recall_score(y_true, y_pred, average=None, labels=labels)
+    f1 = f1_score(y_true, y_pred, average=None, labels=labels)
+
+    i = 0
+    for l in labels:  # loop through each class
+        stats[l] = [precision[i], recall[i], f1[i]]  # get the info for this class specifically
+        i += 1
+
+    return stats  # return the dictionary
+
+
+# testing for confusion matrix - getting F1 score
+# true = ["positive", "neutral", "positive", "positive", "neutral", "negative"]
+# pred = ["neutral", "neutral", "positive", "positive", "neutral", "positive"]
+# print(GetFScores(true, pred))
+
+SplitDataKeepCategories(True)
